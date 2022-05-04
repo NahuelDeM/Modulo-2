1d83aece1667d15890a1201597cfe181a41a8673
NahuelDeM <Bios.Nahuel@gmail.com>
2022-05-04 17:56:30 -0300

    Modulo2

---
+++ Script1.js
@@ -0,0 +1,115 @@
+// Ejercicio 1
+    /* var n1 = parseInt(prompt("Introduce un número"));
+    var n2 = parseInt(prompt("Introduce otro número"));
+
+    var resultado = n1 + n2;
+    alert(resultado); */
+
+// Ejercicio 2
+      /* var n1 = parseInt(prompt("Introduce un número"));
+         var n2 = parseInt(prompt("Introduce otro número"));
+
+         var resultado = n1 + n2;
+         var resultado2 = n1 * n2;
+
+         document.write(resultado + "<br>")
+         document.write(resultado2)
+ */
+// Ejercicio 3
+/*      var n1 = parseInt(prompt("Ingrese un número"));
+        var n2 = parseInt(prompt("Ingrese un número"));
+        var n3 = parseInt(prompt("Ingrese un número"));
+        var n4 = parseInt(prompt("Ingrese un número"));
+        var n5 = parseInt(prompt("Ingrese un número"));
+        
+        var resultado = n1 + n2 + n3 + n4 +n5;
+        var resultadofinal = resultado / 5;
+        window.alert("El promedio es " + resultadofinal)
+ */
+
+// Ejercicio 4
+/*      var n1 = parseInt(prompt("Introduce el largo del rectangulo"));
+        var n2 = parseInt(prompt("Introduce el ancho del rectangulo"));
+
+        var largo = n1 * 2;
+        var ancho = n2 * 2;
+
+        var resultadofinal = largo + ancho;
+        window.alert("El perimetro es " + resultadofinal) */
+
+// Ejercicio 5
+/*  var b = parseInt(prompt("Ingrese la base del triángulo"));
+    var h = parseInt(prompt("Ingrese la altura del triángulo"));
+
+    var resultado = b * h;
+    var division = resultado / 2;
+    window.alert("El area es " + division) */
+
+// Ejercicio 6
+/*      var nombre = prompt("Ingresa tu nombre");
+        var apellido = prompt("Ingresa tu apellido").toUpperCase();
+        var edad = parseInt(prompt("Ingresa tu edad"));
+
+        window.alert("Su nombre es: " + nombre + "\n" + "Su apellido es: " + apellido + "\nSu edad es: " + edad + " años");
+
+// Ejercicio 7
+/*  var n1 = parseInt(prompt("Ingrese el dinero a convertir:"));
+
+    var conversion = n1 * 41.37;
+    window.alert("Este dinero es equivalente a: " + conversion + " Dolares"); */
+
+// Ejercicio 8
+    /* var numero = parseInt(prompt("Ingrese un número: "));
+        
+        if(numero%2==0){
+            alert("El número "+numero+" es par");
+        }else{
+            alert("El número "+numero+" es impar");
+    } */
+
+// Ejercicio 9
+/*          var edad = parseInt(prompt("Ingresa tu año de nacimiento: "));
+
+    if(edad <= 2002){
+        var mensaje = "Puedes votar";
+        window.alert(mensaje);
+    } else {
+        var mensaje = "No puedes votar eres menor";
+        window.alert(mensaje) } */
+
+// Ejercicio 10
+/*     var usuario = prompt("Ingrese su nombre de usuario:");
+    var password = prompt("Ingrese su contraseña:");
+
+    if(password == "provi"){
+    window.alert("Bienvenido Usuario");
+    } else {
+    window.alert("Tu contraseña es incorrecta");
+    } */
+
+// Ejercicio 11
+/*      var n1 = parseInt(prompt("Ingrese un valor: "));
+        var n2 = parseInt(prompt("Ingrese otro valor "));
+
+        var resultado = n1 * n2;
+        var resultado2 = n1 / n2;
+        var resultado3 = n1 - n2;
+        document.write("Los resultados son: " + resultado + "<br>");
+        document.write("Los resultados son: " + resultado2 + "<br>");
+        document.write("Los resultados son: " + resultado3 + "<br>");  */
+
+// Ejercicio 12
+/* var n1 = parseInt(prompt("Introduce uno de los lados del cuadrado: "));
+
+var cuadrado = n1 * 4;
+
+var resultadofinal = cuadrado;
+window.alert("El perimetro es " + resultadofinal); */
+
+// Ejercicio 13
+/* var n1 = parseInt(prompt("Introduce uno de los lados del cuadrado: "));
+
+var cuadrado = n1 * 4;
+
+var resultadofinal = cuadrado;
+window.alert("El perimetro es " + resultadofinal) */
---
+++ Script2.js
@@ -0,0 +1,49 @@
+// Ejercicio 1
+// opcion 1
+/* var n1 = parseInt(prompt("Ingrese un número: "));
+var resultado = Math.abs(n1);
+window.alert(resultado);
+// opcion 2
+
+ */
+// Ejercicio 2
+/* var n1 = parseInt(prompt("Ingrese un número:"));
+        var n2 = parseInt(prompt("Ingrese otro número:"));
+        if(n1 > n2){
+        document.write("El mayor es: " + n1);
+        } else {
+            document.write("El mayor es: " + n2);
+        } */
+
+// Ejercicio 3
+/* var n1 = parseInt(prompt("Ingrese tu edad: "));
+        
+        if(n1 <= 2){ 
+            window.alert("Eres un bebe");
+        } else if (n1 > 2 && n1 <= 10) {
+            window.alert("Eres un niño/a");
+        } else if (n1 >= 10 && n1 <=14){
+            window.alert("Eres un Preadolescente");
+        } else if (n1 >= 15 && n1 <=20){
+            window.alert("Eres un Adolescente");
+        } else if (n1 >=21 && n1 <=25){
+            window.alert("Eres un Joven adulto");
+        } else if (n1 >=26 && n1 <=60){
+            window.alert("Eres un Adulto");
+        } else if (n1 >=61 && n1 <=75){
+            window.alert("Eres un Jubilado");
+        } else if (n1 >=76){
+            window.alert("Eres un Adulto mayor");
+        }
+
+ */
+
+// Ejercicio 4
+/*  var n1 = parseInt(prompt("Ingrese su edad: "));
+    var n2 = confirm("¿Tiene el carne de socio al día?"); 
+
+        if(n1 >=25 && n2) {
+            window.alert("Usted puede votar");
+        } else {
+            window.alert("Usted no puede votar");
+    } */
