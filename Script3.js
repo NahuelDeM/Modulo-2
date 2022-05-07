eb460eb6c1cca08e44002f215601891fed743f5e
NahuelDeM <Bios.Nahuel@gmail.com>
2022-05-07 02:11:29 -0300

    Modulo2

--- Script3.js
+++ Script3.js
@@ -1,6 +1,68 @@
-// Ejercicio 10
+// Ejercicio 1
+/* var rango = parseInt (prompt("ingresa el tope"))
+document.write (parseInt(rango/2)) */
+
+// Ejercicio 2
+/*     edades=[];
+    contador=0
+    do {
+        x = parseInt (prompt('ingresa tu edad'))
+        edades.push(x)
+    }while (x!==0)
+    for (var i = 0; i< edades.length; i++) {
+        if (edades[i] >= 18)
+        contador++
+    }
+    document.write(`entre las edades que ingresaste hay: ${contador} mayores de edad`) */
+
+// Ejercicio 3
+/*  var palabras = prompt("ingresa tu nombre")
+    for (letras of palabras){
+    document.write (`<li> ${letras} <li>`)
+} */
+
+// Ejercicio 4
 /* for (let i = 1; 2**i < 10000; i++) { 
     document.write(2**i + "<br>");               
 } */
 
-// Ejercicio 1
+// Ejercicio 5
+/* var x =0
+for (x==1; x<=10; x++){
+    document.write(`<li> ${x} <li>`) 
+    if(x==5) {
+      break;}
+} */
+
+// Ejercicio 6
+/* var x =0
+for (x==1; x<=10; x++){
+    if (x==5) {
+      continue;}
+    document.write(`<li> ${x} <li>`)} */
+
+// Ejercicio 7
+/* let nombres= [`Eiichirō Oda`, `Akira Toriyama`, `Masashi Kishimoto`, `Naoki Urasawa`, `Kōhei Horikoshi`]
+for  (var i=0; i<=4; i++){
+ document.write (`${nombres[i]} <br>`)
+} */
+
+// Ejercicio 8
+/* let numeros= [1,2,3,4,5,6,7,8,9,10]
+for(var i=0; i<=9; i++) {
+    if (numeros[i] %3==0) {
+    document.write(numeros[i] + "<br>")
+    }
+} */
+
+// Ejercicio 9
+/*  let n1= parseInt(prompt(`ingresa un número`))
+    let n2= parseInt(prompt(`ingresa otro número`))
+    for (var i=n1; i<=n2; i++) {
+    if (i %2==0) {
+    document.write(i + "<br>")
+    }
+} */
+
+// Ejercicio 10
+// Repetido?
